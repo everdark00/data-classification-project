2e6ec3bc88e9508d55e20d4e367c30800f4d57c8
alekseik1 <1alekseik1@gmail.com>
2021-10-23 13:23:41 +0300

    feat: script to copy test data

---
+++ src/make_test_dataset.py
@@ -0,0 +1,40 @@
+from argparse import ArgumentParser
+from collections import defaultdict
+from pathlib import Path
+from shutil import copy2
+
+from loguru import logger
+
+from src.utils import load_config
+
+
+def main(config: dict, test_dir: Path):
+    files_per_dir = 10
+    raw_data_path = Path(config["base"]["data_dir"]) / "raw"
+    new_data_path = test_dir / "raw"
+    logger.info(f"creating dir {new_data_path}")
+    new_data_path.mkdir(parents=True, exist_ok=True)
+    dir_counter = defaultdict(int)
+    for path in raw_data_path.glob("**/*"):
+        new_path = new_data_path / path.relative_to(raw_data_path)
+        if path.is_dir():
+            logger.info(f"creating data dir {new_path}")
+            new_path.mkdir(parents=True, exist_ok=True)
+        else:
+            p_dir = str(path.parent)
+            if dir_counter[p_dir] < files_per_dir:
+                copy2(path, new_path)
+                dir_counter[p_dir] += 1
+
+
+if __name__ == "__main__":
+    a_parser = ArgumentParser()
+    a_parser.add_argument("--config", type=Path, required=True)
+    a_parser.add_argument("--test_dir", type=Path, required=True)
+    args = a_parser.parse_args()
+    config = load_config(args.config)
+    logger.add(
+        Path(config["base"]["log_path"]) / "train_model_{time}.log",
+        level=config["base"]["log_level"],
+    )
+    main(config=config, test_dir=Path(args.test_dir))
